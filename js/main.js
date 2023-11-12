217fe8d4905d7718b5cd4c899dedd908071908dc
Pavel Nesen <nesen.rabota@gmail.com>
2023-11-12 15:45:40 +0300

    update files

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log('Hello world!');
+const blackAwhite = document.querySelector('#blackAwhite');
+const blackBtn = document.querySelector('#black-btn');
+const whiteBtn = document.querySelector('#white-btn');
+const blackSam = document.querySelector('#black-sam');
+const whiteSam = document.querySelector('#white-sam');
+
+function blackAWhite() {
+  function toggleBlackAWhite() {
+    blackBtn.classList.toggle('_active');
+    whiteBtn.classList.toggle('_active');
+    blackSam.classList.toggle('_active-sam');
+    whiteSam.classList.toggle('_active-sam');
+  }
+  blackAwhite.onclick = toggleBlackAWhite;
+}
+blackAWhite();
